2794727c4d262d26e522e508d21264abd233cf99
Purvi Kanal <purvikanal92@gmail.com>
2018-12-13 16:50:02 -0500

    added the homepage

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-export const HomePageTemplate = props => <div>Home!</div>;
+export const HomePageTemplate = ({ title }) => <h1>{title}</h1>;
 
-const HomePage = ({ data }) => (
-  <Layout>
-    <HomePageTemplate />
-  </Layout>
-);
+const HomePage = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <HomePageTemplate title={post.frontmatter.title} />
+    </Layout>
+  );
+};
 
 export default HomePage;
+
+export const homePageQuery = graphql`
+  query HomePage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
